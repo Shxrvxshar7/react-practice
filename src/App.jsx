42356ef4c917334e4d201747834a1ec1a2dac85e
Shxrvxshar7 <arsharvesh7@gmail.com>
2026-05-07 19:07:14 +0530

    Conditional rendering both if and ternary

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* 
+
 import { useState } from 'react'
 import Student from './Student.jsx'
 
@@ -6,9 +8,26 @@ function App() {
   const [count, setCount] = useState(0)
   return (
     <>
-      { <Student name = "Spongebob" age = {30} isStudent ={true} />   /* this key value pair will be sent to the props object in Student.jsx */}
+      {<Student name = "Spongebob" age = "35" isStudent ={true} /> }
       {<Student name = "Patrick" age = {32} isStudent ={false}/>}
       {<Student name = "Sandy" age = {25} isStudent ={true}/>}
+      {<Student/>}
+      
+    </>
+  )
+}
+
+export default App
+
+*/ 
+
+
+import UserGreeting from "./UserGreeting.jsx"
+
+function App() {
+  return(
+    <>
+      <UserGreeting isLoggedIn = {false} username="Sharvesh"/>
     </>
   )
 }
--- src/Student.jsx
+++ src/Student.jsx
@@ -1,4 +1,9 @@
+import { Component } from "react"
+import PropTypes from "prop-types"
 
+// Props = read-only properties that are shared between components
+    //    A parent component can send data to a child component. 
+    //    <Component key = value />
 
 function Student(props) {
     return(
@@ -10,5 +15,23 @@ function Student(props) {
         </div>
     )
 }
+//propTypes = checks if the passed  value is the correct datatype, age:PropTypes.number
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
+
+}
+
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false
+}
 
 export default Student
+
+
+
+
+
